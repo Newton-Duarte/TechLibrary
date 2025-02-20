26d8f9cfce32f24922df60a49eb7f326bc94d85c
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-02-20 16:27:24 -0300

    chore: fix users controller register method

--- TechLibrary.Api/Controllers/UsersController.cs
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -4,44 +4,43 @@ using TechLibrary.Communication.Requests;
 using TechLibrary.Communication.Responses;
 using TechLibrary.Exception;
 
-namespace TechLibrary.Api.Controllers
+namespace TechLibrary.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseUserJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status400BadRequest)]
+    public IActionResult Register(RequestUserJson request)
     {
-        [HttpPost]
-        [ProducesResponseType(typeof(ResponseUserJson), StatusCodes.Status201Created)]
-        [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status400BadRequest)]
-        public IActionResult Create(RequestUserJson request)
+        try
         {
-            try
-            {
-                var useCase = new RegisterUserUseCase();
-                var response = useCase.Execute(request);
+            var useCase = new RegisterUserUseCase();
+            var response = useCase.Execute(request);
 
-                return Created(string.Empty, response);
-            }
-            catch (TechLibraryException ex)
-            {
-                return BadRequest(new ResponseErrorMessagesJson
-                {
-                    ErrorMessages = ex.GetErrorMessages()
-                });
-            }
-            catch
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorMessagesJson
-                {
-                    ErrorMessages = new List<string> { "An error occurred while processing the request." }
-                });
-            }
+            return Created(string.Empty, response);
         }
-
-        [HttpGet]
-        public IActionResult Get()
+        catch (TechLibraryException ex)
         {
-            return Ok();
+            return BadRequest(new ResponseErrorMessagesJson
+            {
+                ErrorMessages = ex.GetErrorMessages()
+            });
         }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorMessagesJson
+            {
+                ErrorMessages = new List<string> { "An error occurred while processing the request." }
+            });
+        }
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
     }
 }
