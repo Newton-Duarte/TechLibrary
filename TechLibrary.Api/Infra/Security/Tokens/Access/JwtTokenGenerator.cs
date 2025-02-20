c1b797196a924e5615d2db5f9403ef92b9aea61c
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-02-20 17:04:19 -0300

    feat: add jwt token generator class

---
+++ TechLibrary.Api/Infra/Security/Tokens/Access/JwtTokenGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.Infra.Security.Tokens.Access;
+
+public class JwtTokenGenerator
+{
+    public string Generate(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(60),
+            Subject = new ClaimsIdentity(claims),
+            SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    private SymmetricSecurityKey SecurityKey()
+    {
+        var signingKey = "OMgkb6glnxjGd8j8W7wSN9IHphXG5pjA";
+
+        var symmetricKey = Encoding.UTF8.GetBytes(signingKey);
+
+        return new SymmetricSecurityKey(symmetricKey);
+    }
+}
