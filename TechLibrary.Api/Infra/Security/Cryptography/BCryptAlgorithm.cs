48409d5b780f04f8e9a48f4dda11b84cee4c2206
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-02-20 16:32:32 -0300

    feat: add bcrypt algorithm class

---
+++ TechLibrary.Api/Infra/Security/Cryptography/BCryptAlgorithm.cs
@@ -0,0 +1,6 @@
+﻿namespace TechLibrary.Api.Infra.Security.Cryptography;
+
+public class BCryptAlgorithm
+{
+    public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+}
