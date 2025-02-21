fb4a7e8bc9ea7555c1cf841a5899ed7db100e328
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-02-21 10:46:21 -0300

    feat: add books controller

---
+++ TechLibrary.Api/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Api.UseCases.Books.List;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+
+namespace TechLibrary.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseListBooksJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseListBooksJson), StatusCodes.Status204NoContent)]
+    public IActionResult Get(int page, string? title)
+    {
+        var useCase = new ListBooksUseCase();
+        var result = useCase.Execute(new RequestBooksJson
+        {
+            Page = page,
+            Title = title
+        });
+
+        return Ok(result);
+    }
+}
