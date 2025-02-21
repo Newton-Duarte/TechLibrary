56bbb3fa1cce4cef5f04fe361e4abeea86d3d84d
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-02-21 11:38:55 -0300

    feat: add checkouts controller

---
+++ TechLibrary.Api/Controllers/CheckoutsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Api.Services.LoggedUser;
+using TechLibrary.Api.UseCases.Checkouts;
+
+namespace TechLibrary.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CheckoutsController : ControllerBase
+{
+    [HttpPost]
+    [Route("{bookId}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult BookCheckout(Guid bookId)
+    {
+        var loggedUserService = new LoggedUserService(HttpContext);
+
+        var useCase = new RegisterCheckoutUseCase(loggedUserService);
+
+        useCase.Execute(bookId);
+
+        return Created("", null);
+    }
+}
