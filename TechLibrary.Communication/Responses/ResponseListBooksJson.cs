b2c1f51bbd5ddca8551480ac72bff52f61fe80d2
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-02-21 10:45:56 -0300

    feat: add response list books json class

---
+++ TechLibrary.Communication/Responses/ResponseListBooksJson.cs
@@ -0,0 +1,7 @@
+﻿namespace TechLibrary.Communication.Responses;
+
+public class ResponseListBooksJson
+{
+    public ResponsePaginationJson Pagination { get; set; } = default!;
+    public List<ResponseBookJson> Books { get; set; } = [];
+}
